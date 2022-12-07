1d7171cbe51ad1d9aad65ebb32f97cc1027bd13a
Griffin <ffirgn@gmail.com>
2022-12-06 16:12:45 -0800

    added questions logic to index

--- index.js
+++ index.js
@@ -6,7 +6,54 @@ const generateMarkdown = require('./utils/generateMarkdown');
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project? (Required)',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please provide a description of your project. (Required)',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Please provide any installation instructions here.',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Please provide the main use case for your project/application here.',
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'Please provide contribution guidelines for users that contribute to your project/application.',
+    },
+    {
+      type: 'input',
+      name: 'test',
+      message: 'Which test have you performed that you would like to include in your README?',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Which license would you like to use for your project?',
+      choices: ['Apache 2.0', 'BSD 3', 'GNU GPL', 'MIT', 'None']
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is your GitHub username? (Required)',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address? (Required)',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
