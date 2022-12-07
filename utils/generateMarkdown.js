27cca7f974090c4a017fad6ce283dfb1752b4c2a
Griffin <ffirgn@gmail.com>
2022-12-06 16:48:48 -0800

    added logic for license area, and generateMarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,67 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'None'){
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-black.svg)`
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'None'){
+    return `\n* [License](#license)\n`;
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return `## License
+    Project currently licensed under the ${license} license.`;
+  }
+  return '';
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+## Description
+${data.description}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contribution](#contribution)
+* [Tests](#tests)
+${renderLicenseLink(data.license)}
+* [Contact](#contact)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Contribution
+
+${data.contribution}
+
+## Tests
+
+${data.tests}
+
+${renderLicenseSection(data.license)}
+${renderLicenseBadge(data.license)}
+
+## Contact
+
+To contact me regarding this project, please reach out to me at ${data.email} or visit my GitHub profile at ${data.github}.
 
 `;
 }
