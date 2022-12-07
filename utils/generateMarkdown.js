86c8f465e9791b8de4fd42d885a17a27eb36516a
Griffin <ffirgn@gmail.com>
2022-12-06 17:37:50 -0800

    Revert "removing test readme file created during testing"
    This reverts commit b8474d46400e98c989853eec0be50b1d540baa2f.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -29,7 +29,6 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
 ## Description
 ${data.description}
 
